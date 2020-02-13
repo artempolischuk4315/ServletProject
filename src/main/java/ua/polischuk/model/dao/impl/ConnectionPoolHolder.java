aea3567d8856f0010422b238957126485e980f75
Artem Polishchuk <art4315@gmail.com>
2020-02-13 23:27:13 +0200

    git ignore

--- src/main/java/ua/polischuk/model/dao/impl/ConnectionPoolHolder.java
+++ src/main/java/ua/polischuk/model/dao/impl/ConnectionPoolHolder.java
@@ -21,7 +21,6 @@ public class ConnectionPoolHolder {
         ds.setDriverClassName(dbData.getString("db.driver"));
         ds.setMinIdle(Integer.parseInt(dbData.getString("db.minIdle")));
         ds.setMaxIdle(Integer.parseInt(dbData.getString("db.maxIdle")));
-        //ds.setMaxIdle(1);
         ds.setMaxOpenPreparedStatements(Integer.parseInt(dbData.getString("db.maxOpenStatement")));
         this.dataSource = ds;
     }
@@ -50,35 +49,4 @@ public class ConnectionPoolHolder {
         }
     }
 
-    /*public static DataSource getDataSource(){
-        if (dataSource == null){
-            synchronized (ConnectionPoolHolder.class) {
-                if (dataSource == null) {
-
-                    BasicDataSource ds = new BasicDataSource();
-                    ds.setUrl("jdbc:mysql://localhost:3306/myservletdb?useUnicode=yes&characterEncoding=UTF-8");
-                    ds.setUsername("root");
-                    ds.setPassword("1");
-                    ds.setDriverClassName("com.mysql.jdbc.Driver");
-                    ds.setMinIdle(5);
-                    ds.setMaxIdle(10);
-                    ds.setMaxOpenPreparedStatements(100);
-                    dataSource = ds;
-                }
-            }
-        }
-        return dataSource;
-
-    }
-
-    public final Connection getConnection() {
-        //logger.info("connect");
-        try {
-            return this.dataSource.getConnection();
-        } catch (SQLException e) {
-            System.out.println("connect catch");
-            //logger.info("connection error", e);
-            throw new RuntimeException(e);
-        }
-    }*/
 }
